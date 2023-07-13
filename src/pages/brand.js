04d9959a312d4670f8ed6bdae8ac6cf682d60c4e
DylanHenstock <dylanhenstock@Dylans-MacBook-Air.local>
2023-07-13 15:22:39 +0200

    block style

--- src/pages/brand.js
+++ src/pages/brand.js
@@ -1,7 +1,28 @@
 export default function Brand () {
     return (
-        <div class="block humanexperience">
-            <h2>Brand Page</h2>
+        <div class="container">
+            <div class="block humanexperience">
+                <h1>The Human Experience</h1>
+            </div>
+
+            <div class="block creatorjams">
+                <h1>CreatorJams</h1>
+            </div>
+
+            <div class="block dynemedia">
+                <h1>Dyne Media Group</h1>
+            </div>
+
+            <div class="block outfield">
+                <h1>Outfield</h1>
+            </div>
+
+            <div class="block dizzio">
+                <h1>Dizzio AI</h1>
+            </div>
+            
         </div>
+        
+        
     )
 }
