f259fb176ea4dc571fd91817f7238948284e5900
DylanHenstock <dylanhenstock@Dylans-MacBook-Air.local>
2023-07-21 12:30:17 +0200

    First Block

--- src/pages/web.js
+++ src/pages/web.js
@@ -1,3 +1,6 @@
+import creatorjamslogo from '../img/creatorjams.svg'
+import outfieldlogo from '../img/outfield.svg'
+
 export default function Web () {
     return (
         <div class="container">
@@ -5,12 +8,34 @@ export default function Web () {
             {/* CreatorJams */}
 
             <div class="block creatorjams">
-                <h2>Product Page</h2>
+                <img src={creatorjamslogo} alt="CreatorJams Logo"/>
+                <h1>CreatorJams</h1>
+                <p class="subtitle">An online store selling Music, Sound Effects & 
+                <br />Visual Presets to Content Creators</p>
+                <div class="rolesgrid">
+                    <div class="grid-1">
+                        <h3>Role</h3>
+                        <p>UI/UX Desinger & <br /> Wordpress Developer</p>
+                    </div>
+                    <div class="grid-2">
+                        <h3>Tools</h3>
+                        <p>Figma, Wordpress, HTML, CSS</p>
+                    </div>
+                    <div class="grid-3">
+                        <h3>Scope</h3>
+                        <p>April 2021 - Present<br />Personal Project</p>
+                    </div>
+                    <div class="grid-4">
+                        <h3>What I did</h3>
+                        <p>Competitor Analysis, User Research<br />Mobile & Desktop Designs</p>
+                    </div>
+
+                </div>
             </div>
 
             <div class="grid">
                 <div class="grid-left">
-                    <h2>Project</h2>
+                    <h2>Overview 🏠</h2>
                 </div>
 
                 <div class="grid-right creatorjams">
@@ -21,12 +46,15 @@ export default function Web () {
            {/* Outfield */}
 
            <div class="block outfield">
-                <h2>Product Page</h2>
+                 <img src={outfieldlogo} alt="Outfield Logo"/>
+                <h1>Outfield</h1>
+                <p class="subtitle">An Agritech Start up that uses <strong>Drones</strong> and <strong>AI</strong>
+                <br />to maximise profits on Apple Farms</p>
             </div>
 
             <div class="grid">
                 <div class="grid-left">
-                    <h2>Project</h2>
+                    <h2>Overview 🏠</h2>
                 </div>
 
                 <div class="grid-right outfield">
