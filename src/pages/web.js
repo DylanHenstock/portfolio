2546dbec837d9af37b20228f1463e73c7cdf57a0
DylanHenstock <dylanhenstock@Dylans-MacBook-Air.local>
2023-07-25 16:27:12 +0200

    column info filled

--- src/pages/web.js
+++ src/pages/web.js
@@ -1,5 +1,8 @@
 import creatorjamslogo from '../img/creatorjams.svg'
 import outfieldlogo from '../img/outfield.svg'
+import creatorjams1 from '../img/creatorjams1.jpg'
+import creatorjams2 from '../img/creatorjams2.jpg'
+import creatorjams3 from '../img/creatorjams3.jpg'
 
 export default function Web () {
     return (
@@ -29,7 +32,6 @@ export default function Web () {
                         <h3>What I did</h3>
                         <p>Competitor Analysis, User Research<br />Mobile & Desktop Designs</p>
                     </div>
-
                 </div>
             </div>
 
@@ -39,7 +41,9 @@ export default function Web () {
                 </div>
 
                 <div class="grid-right creatorjams">
-                    <h2>Image</h2>
+                    <img src={creatorjams1} alt="CreatorJams Home Page"/>
+                    <img src={creatorjams2} alt="CreatorJams Shop Page"/>
+                    <img src={creatorjams3} alt="CreatorJams Music Page"/>
                 </div>
             </div>
 
@@ -50,6 +54,24 @@ export default function Web () {
                 <h1>Outfield</h1>
                 <p class="subtitle">An Agritech Start up that uses <strong>Drones</strong> and <strong>AI</strong>
                 <br />to maximise profits on Apple Farms</p>
+                <div class="rolesgrid">
+                    <div class="grid-1">
+                        <h3>Role</h3>
+                        <p>UI/UX Desinger & <br /> Wordpress Developer</p>
+                    </div>
+                    <div class="grid-2">
+                        <h3>Tools</h3>
+                        <p>Figma, Wordpress, HTML, CSS</p>
+                    </div>
+                    <div class="grid-3">
+                        <h3>Scope</h3>
+                        <p>April 2021 - Present<br />Personal Project</p>
+                    </div>
+                    <div class="grid-4">
+                        <h3>What I did</h3>
+                        <p>Competitor Analysis, User Research<br />Mobile & Desktop Designs</p>
+                    </div>
+                </div>
             </div>
 
             <div class="grid">
@@ -58,7 +80,7 @@ export default function Web () {
                 </div>
 
                 <div class="grid-right outfield">
-                    <h2>Image</h2>
+                   <h2>Images</h2>
                 </div>
             </div>
 
